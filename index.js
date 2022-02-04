462d805a295ea3d3a450c02c0a7b9434a73f6f76
candacehernandez <chernandez.aggie@gmail.com>
2022-02-03 21:57:03 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+//customreName
+var customerName = "bob";
+
+//upperCaseCustomerName()
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+   
+    return customerName;
+}
+
+//setBestCustomer()
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+//changeLeastFavoriteCustomer()
+
+const leastFavoriteCustomer = 'tod';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'TOD';
+    return leastFavoriteCustomer;
+}
